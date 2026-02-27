ea83df05bde83aaca3ffffb323e87539c70a1105
Marconi Andrade <marconiandradefilho@gmail.com>
2026-02-27 00:21:14 +0000

    .

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       Olá, meus amigos, quando nos veremos?
+      <Profile />
+      Exemplo de expressão: {5**3}
     </div>
   );
 }
