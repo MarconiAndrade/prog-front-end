119ae1e759a44f99d91c38dda2580b15e9ffadc3
Marconi Andrade <marconiandradefilho@gmail.com>
2026-04-06 13:09:53 +0000

    expressão Math.random() do valor dos dados

---
+++ jogo-dados/components/JogoDados.js
@@ -0,0 +1,9 @@
+"use client";
+import { useState } from 'react';
+import Dado from './Dado';
+
+export default function JogoDados(){
+    const numeroDado = Math.floor(Math.random()*6) + 1;
+
+    
+}
